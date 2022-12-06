c7aba8048e778a587f2dac15e6b5760a3a8b5e9e
danyeric123 <dnagarpowers@gmail.com>
2022-12-06 17:17:24 -0500

    solution to day 6

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 My solutions to adventofcode.com puzzles.
 
+_Some of the solutions are taken from the subreddit for AOC 2022 since some days
+I didn't have so much time to devote to coming up with my own solution_
+
 :warning: I make no guarentee that my answer for the day is the most efficient
---
+++ supply_stacks/main.py
@@ -0,0 +1,21 @@
+def solve(stacks, lines, step=1):
+    for count, src, dst in lines:
+        stacks[dst] += stacks[src][-count:][::step]
+        stacks[src] = stacks[src][:-count]
+
+    return "".join(s[-1] for s in stacks)
+
+
+data, lines = open("./supply_stacks/input.txt").read().split("\n\n")
+stacks = [
+    "".join(column).rstrip()
+    for i, column in enumerate(zip(*data.splitlines()[-2::-1]))
+    if i % 4 == 1
+]
+lines = [
+    (int(line[1]), int(line[3]) - 1, int(line[5]) - 1)
+    for line in map(str.split, lines.splitlines())
+]
+
+print(solve(stacks.copy(), lines, -1))
+print(solve(stacks.copy(), lines))
